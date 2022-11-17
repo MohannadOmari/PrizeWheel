6d07c7eb9c38d71423ab7b709ecc648220ed3516
Mohannad <mohannad.alumari@gmail.com>
2022-11-17 14:10:02 +0300

    Prize Wheel Project start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UmniahPrizeWheel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
